39f077fe5e0bee76a196b54ac30cf956f52232a2
zehreken <zehreken@gmail.com>
2020-06-25 23:37:14 +0200

    Add basic input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use console::style;
+use console::Key;
+
 fn main() {
-    println!("Hello, world!");
+    let term = console::Term::stdout();
+    
+    let mut res_key;
+    'running: loop {
+        res_key = term.read_key();
+        match res_key.unwrap() {
+            Key::Char(c) => test_key(c),
+            Key::Enter => println!("enter"),
+            Key::Backspace => println!("backspace"),
+            Key::Escape => break 'running,
+            _ => {},
+        }
+    } // 'running
+}
+
+fn test_key(c: char) {
+    if c == 'a' {
+        println!("test a");
+    } else if c == 's' {
+        println!("test s");
+    } else if c == 'A' {
+        println!("test A");
+    }
 }
