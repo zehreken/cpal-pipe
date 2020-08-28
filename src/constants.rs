261f03305c24001d5a6162507be3877485d6dd1c
zehreken <zehreken@gmail.com>
2020-08-28 20:29:08 +0200

    Refactor and simplify

--- src/constants.rs
+++ src/constants.rs
@@ -3,3 +3,4 @@ pub const _RED: &str = "\x1b[0;41m";
 pub const _GREEN: &str = "\x1b[0;32m";
 pub const _YELLOW: &str = "\x1b[0;33m";
 pub const BUFFER_CAPACITY: usize = 4096;
+pub const FILLER: u32 = 32;
--- src/pipe.rs
+++ src/pipe.rs
@@ -36,10 +36,6 @@ pub fn start_play_through(receiver: Receiver<usize>) {
             index = receiver.recv().unwrap();
         }
         let input_device: &Device = &input_devices[index];
-        let input_channel_count = match input_device.default_input_config() {
-            Ok(f) => f.channels(),
-            Err(_) => 0,
-        };
 
         // Fetch output devices
         let output_devices = cpal_utils::get_output_devices(&host);
@@ -69,33 +65,17 @@ pub fn start_play_through(receiver: Receiver<usize>) {
             index = receiver.recv().unwrap();
         }
         let output_device: &Device = &output_devices[index];
-        let output_channel_count = match output_device.default_output_config() {
-            Ok(f) => f.channels(),
-            Err(_) => 0,
-        };
 
-        let (prod_factor, cons_factor) =
-            get_channel_factor(input_channel_count, output_channel_count);
-
-        // println!("{}, {}", prod_factor, cons_factor);
         let ring_buffer = RingBuffer::new(constants::BUFFER_CAPACITY);
         let (mut prod, mut cons) = ring_buffer.split();
-        for _ in 0..10 {
+        for _ in 0..constants::FILLER {
             prod.push(0.0).unwrap();
         }
 
         let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
-            let mut output_fell_behind = false;
             for &sample in data {
-                for _ in 0..prod_factor {
-                    if prod.push(sample).is_err() {
-                        output_fell_behind = true;
-                    }
-                }
+                prod.push(sample).unwrap();
             }
-            // if output_fell_behind {
-            //     eprintln!("Output stream fell behind")
-            // }
         };
 
         let config: cpal::StreamConfig = input_device.default_input_config().unwrap().into();
@@ -104,21 +84,12 @@ pub fn start_play_through(receiver: Receiver<usize>) {
             .unwrap();
 
         let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-            let mut input_fell_behind = false;
             for sample in data {
-                for _ in 0..cons_factor {
-                    *sample = match cons.pop() {
-                        Some(s) => s,
-                        None => {
-                            input_fell_behind = true;
-                            0.0
-                        }
-                    };
-                }
+                *sample = match cons.pop() {
+                    Some(s) => s,
+                    None => 0.0,
+                };
             }
-            // if input_fell_behind {
-            //     eprintln!("Input stream fell behind");
-            // }
         };
 
         let output_stream = output_device
@@ -132,32 +103,12 @@ pub fn start_play_through(receiver: Receiver<usize>) {
             output_stream
                 .play()
                 .expect("Error while playing output stream");
+
+            std::thread::sleep(std::time::Duration::from_millis(16));
         }
     });
 }
 
 fn err_fn(err: cpal::StreamError) {
     eprintln!("an error occurred on stream: {}", err);
-}
-
-fn get_channel_factor(input_channel_count: u16, output_channel_count: u16) -> (u16, u16) {
-    let mut prod_factor = 0;
-    let mut cons_factor = 0;
-    if input_channel_count == output_channel_count
-        || (input_channel_count == 0 || output_channel_count == 0)
-    {
-        prod_factor = 1;
-        cons_factor = 1;
-    } else if input_channel_count != output_channel_count {
-        if input_channel_count > output_channel_count {
-            prod_factor = 1;
-            cons_factor = input_channel_count / output_channel_count;
-        } else {
-            prod_factor = output_channel_count / input_channel_count;
-            cons_factor = 1;
-        }
-    }
-    prod_factor = 1;
-    cons_factor = 1;
-    (prod_factor, cons_factor)
 }
